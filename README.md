c02e73d0b0c016ad6568c57ef361b0ea01d4dc29
Bread-Ch4n <94946192+Bread-Ch4n@users.noreply.github.com>
2025-10-03 18:53:55 +0300

    feat(v1.0.0): initial release
    Initial release of the mod

---
+++ README.md
@@ -0,0 +1,6 @@
+<h1 align="center">Single Food In Feeder</h1>
+<h3 align="center">Single Food In Feeders is a mod for Slime Rancher 2 that makes the feeder keep 1 item in them.</h3>
+
+<p align="center">
+  <a href="https://www.nexusmods.com/slimerancher2/mods/109"><img src="https://img.shields.io/badge/Vacuum Modifications-232634?style=for-the-badge&logo=nexus-mods&logoColor=232634&color=D98F40" alt="Vacuum Modifications"></a>
+</p>
---
+++ src/Mod.cs
@@ -0,0 +1,45 @@
+﻿using HarmonyLib;
+using Il2Cpp;
+using MelonLoader;
+using SingleFoodInFeeder;
+
+[assembly: MelonInfo(
+    typeof(Mod),
+    "SingleFoodInFeeder",
+    "1.0.0",
+    "Bread-Chan",
+    "https://www.nexusmods.com/slimerancher2/mods/109"
+)]
+[assembly: MelonGame("MonomiPark", "SlimeRancher2")]
+
+namespace SingleFoodInFeeder;
+
+public class Mod : MelonMod
+{
+    #region Patches
+
+    [HarmonyPatch(typeof(SlimeFeeder), nameof(SlimeFeeder.ProcessFeedOperation))]
+    [HarmonyPrefix]
+    private static bool SlimeFeeder_ProcessFeedOperation(SlimeFeeder __instance)
+    {
+        if (__instance._storage.Ammo.Slots[0].Count > 1)
+            return true;
+        var model = __instance._model;
+        model.remainingFeedOperations = Math.Max(0, model.remainingFeedOperations - 1);
+        return false;
+    }
+
+    #endregion
+
+    #region Setup
+
+    public override void OnInitializeMelon()
+    {
+        var h = new HarmonyLib.Harmony("com.bread-chan_single_food_in_feeder");
+        h.PatchAll(typeof(Mod));
+
+        MelonLogger.Msg("Initialized.");
+    }
+
+    #endregion
+}
